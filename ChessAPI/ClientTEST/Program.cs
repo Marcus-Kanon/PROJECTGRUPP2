3dae10fa8884890ac396923decd3b1c3fa907690
Marcus-Kanon <94marsun@gafe.molndal.se>
2022-02-15 17:58:00 +0100

    Fungerar från console client nu

---
+++ ChessAPI/ClientTEST/Program.cs
@@ -0,0 +1,15 @@
+﻿using ClientTEST;
+
+var APIHandler = new SelectAPI();
+
+while(true)
+{
+    APIHandler.Menu();
+    APIHandler.Act();
+
+    Console.WriteLine("GameId: " + APIHandler.Game.GameId);
+    Console.WriteLine("Player1Id: " + APIHandler.Game.Player1Id);
+    Console.WriteLine("Player2Id: " + APIHandler.Game.Player2Id);
+}
+
+
