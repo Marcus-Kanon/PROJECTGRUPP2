735ead7ea30835c8b6f8ea48063f8b5a67edc285
Marcus-Kanon <94marsun@gafe.molndal.se>
2022-02-15 11:00:34 +0100

    Start

---
+++ ChessAPI/ChessAPI/Controllers/GetBoard.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ChessAPI.Controllers
+{
+    
+    [ApiController]
+    public class GetBoard : ControllerBase
+    {
+        // GET: api/<GetBoard>
+        [Route("api/[controller]")]
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
