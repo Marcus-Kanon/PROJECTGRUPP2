0e3e9ff1c69da056e527c95b5995b15b13ac0ce4
Marcus-Kanon <94marsun@gafe.molndal.se>
2022-02-15 16:38:11 +0100

    Den sparar spelen på RAM och serialiserar objekten nu

--- ChessAPI/ChessAPI/Game.cs
+++ ChessAPI/ChessAPI/Game.cs
@@ -2,10 +2,17 @@
 {
     public class Game
     {
-        IGamePiece[,] Board { get; set; }
-        public string MatchId { get; set; }
+        const int BOARD_WIDTH = 8;
+
+        public IGamePiece[,] Board { get; set; }
+        public string GameId { get; set; }
         public string Player1Id { get; set; }
         public string Player2Id { get; set; }
-        bool ToMovePlayer1 { get; set; }
+        public string PlayerTurnId { get; set; }
+
+        public Game()
+        {
+            Board = new IGamePiece[BOARD_WIDTH, BOARD_WIDTH];
+        }
     }
 }
