49893b2d2075a1d2a72e377162b1e899363dfd8e
Marcus-Kanon <94marsun@gafe.molndal.se>
2022-02-15 11:09:53 +0100

    Grund

---
+++ ChessAPI/ChessAPI/Game.cs
@@ -0,0 +1,11 @@
+﻿namespace ChessAPI
+{
+    public class Game
+    {
+        GamePieces[,] Board { get; set; }
+        public string MatchId { get; set; }
+        public string Player1Id { get; set; }
+        public string PlayerId { get; set; }
+        bool ToMovePlayer1 { get; set; }
+    }
+}
---
+++ ChessAPI/ChessAPI/GamePieces.cs
@@ -0,0 +1,7 @@
+﻿namespace ChessAPI
+{
+    public interface GamePieces
+    {
+        string Move((int x, int y), (int x2, int y2));
+    }
+}
