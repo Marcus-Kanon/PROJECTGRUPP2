7b7d3a9d88e977158f3d6316cf6163f90705cb2a
JT <95jultan@gafe.molndal.se>
2022-02-26 16:15:32 +0100

    pawn, queen, rook tests all pass. Lagt till att queen, rook får illegal move message om försöker röra till samma plats som den själv står på

--- ChessAPI/ChessAPI Tests/GamePieces/PawnTests.cs
+++ ChessAPI/ChessAPI Tests/GamePieces/PawnTests.cs
@@ -22,12 +22,16 @@ namespace ChessAPI.GamePieces.Tests
             newGame = gamesService.CreateNewGame();
             
             customGame = gamesService.CreateNewGame();
-            customGame.Board = new GamePiece[4, 4]
+            customGame.Board = new GamePiece[8, 8]
             {
-                { new Pawn(customGame, Color.Light), new NoPiece(customGame, Color.Empty), new Pawn(customGame, Color.Dark), new Pawn(customGame, Color.Dark) },
-                { new Pawn(customGame, Color.Light), new NoPiece(customGame, Color.Empty), new Pawn(customGame, Color.Dark), new Pawn(customGame, Color.Dark) },
-                { new Pawn(customGame, Color.Light), new Pawn(customGame, Color.Light), new NoPiece(customGame, Color.Empty), new Pawn(customGame, Color.Dark) },
-                { new Pawn(customGame, Color.Light), new Pawn(customGame, Color.Light), new NoPiece(customGame, Color.Empty), new Pawn(customGame, Color.Dark) },
+                { new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty) },
+                { new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty) },
+                { new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new Pawn(customGame, Color.Light), new NoPiece(customGame, Color.Empty), new Pawn(customGame, Color.Dark), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty) },
+                { new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new Pawn(customGame, Color.Light), new NoPiece(customGame, Color.Empty), new Pawn(customGame, Color.Dark), new Pawn(customGame, Color.Dark), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty) },
+                { new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new Pawn(customGame, Color.Light), new Pawn(customGame, Color.Light), new NoPiece(customGame, Color.Empty), new Pawn(customGame, Color.Dark), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty) },
+                { new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new Pawn(customGame, Color.Light), new NoPiece(customGame, Color.Empty), new Pawn(customGame, Color.Dark), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty) },
+                { new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty) },
+                { new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty) }
             };
         }
 
@@ -54,7 +58,7 @@ namespace ChessAPI.GamePieces.Tests
         [TestMethod()]
         [DataRow(0, 1, 0, 2, MoveValidationMessage.Succeeded)]
         [DataRow(0, 1, 0, 3, MoveValidationMessage.Succeeded)]
-        public void MoveTest_LightPawnMovesOneSquareOrTwoSquaresForward_ReturnsSucceedede(int oldCol, int oldRow, int newCol, int newRow, MoveValidationMessage expected)
+        public void MoveTest_LightPawnMovesOneSquareOrTwoSquaresForwardOnFirstMove_ReturnsSucceedede(int oldCol, int oldRow, int newCol, int newRow, MoveValidationMessage expected)
         {
             var actual = newGame?.Board?[0,1].Move((oldCol, oldRow), (newCol, newRow));
             Assert.AreEqual(expected, actual);
@@ -63,7 +67,7 @@ namespace ChessAPI.GamePieces.Tests
         [TestMethod()]
         [DataRow(0, 6, 0, 5, MoveValidationMessage.Succeeded)]
         [DataRow(0, 6, 0, 4, MoveValidationMessage.Succeeded)]
-        public void MoveTest_DarkPawnMovesOneSquareOrTwoSquaresForward_ReturnsSucceeded(int oldCol, int oldRow, int newCol, int newRow, MoveValidationMessage expected)
+        public void MoveTest_DarkPawnMovesOneSquareOrTwoSquaresForwardOnFirstMove_ReturnsSucceeded(int oldCol, int oldRow, int newCol, int newRow, MoveValidationMessage expected)
         {
             newGame.Player1 = new() { Color = Color.Light, IsPlayerTurn = false };
             newGame.Player2 = new() { Color = Color.Dark, IsPlayerTurn = true };
@@ -72,24 +76,48 @@ namespace ChessAPI.GamePieces.Tests
         }
 
         [TestMethod()]
-        [DataRow(0, 1, 0, 0, MoveValidationMessage.IllegalMove)]
-        [DataRow(0, 1, 1, 1, MoveValidationMessage.IllegalMove)]
-        [DataRow(0, 1, 1, 2, MoveValidationMessage.IllegalMove)]
-        public void MoveTest_LightPawnMovesBackwardsOrSidewaysOrDiagonally_ReturnsIllegalMove(int oldCol, int oldRow, int newCol, int newRow, MoveValidationMessage expected)
+        [DataRow(5, 3, 5, 3, MoveValidationMessage.IllegalMove)]
+        [DataRow(5, 3, 5, 2, MoveValidationMessage.IllegalMove)]
+        [DataRow(4, 3, 3, 3, MoveValidationMessage.IllegalMove)]
+        [DataRow(4, 2, 3, 3, MoveValidationMessage.IllegalMove)]
+        [DataRow(4, 2, 4, 3, MoveValidationMessage.IllegalMove)]
+        public void MoveTest_LightPawnMovesToSameSpotOrBackwardsOrSidewaysOrDiagonallyOrPathBlocked_ReturnsIllegalMove(int oldCol, int oldRow, int newCol, int newRow, MoveValidationMessage expected)
         {
             var actual = customGame?.Board?[0, 1].Move((oldCol, oldRow), (newCol, newRow));
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod()]
-        [DataRow(3, 2, 3, 3, MoveValidationMessage.IllegalMove)]
-        [DataRow(3, 3, 2, 3, MoveValidationMessage.IllegalMove)]
-        [DataRow(3, 3, 2, 2, MoveValidationMessage.IllegalMove)]
-        public void MoveTest_DarkPawnMovesBackwardsOrSidewaysOrDiagonally_ReturnsIllegalMove(int oldCol, int oldRow, int newCol, int newRow, MoveValidationMessage expected)
+        [DataRow(2, 4, 2, 4, MoveValidationMessage.IllegalMove)]
+        [DataRow(2, 4, 2, 5, MoveValidationMessage.IllegalMove)]
+        [DataRow(3, 4, 4, 4, MoveValidationMessage.IllegalMove)]
+        [DataRow(2, 4, 3, 3, MoveValidationMessage.IllegalMove)]
+        [DataRow(3, 5, 3, 4, MoveValidationMessage.IllegalMove)]
+        public void MoveTest_DarkPawnMovesToSameSpotBackwardsOrSidewaysOrDiagonallyOrPathBlocked_ReturnsIllegalMove(int oldCol, int oldRow, int newCol, int newRow, MoveValidationMessage expected)
         {
             customGame.Player1 = new() { Color = Color.Light, IsPlayerTurn = false };
             customGame.Player2 = new() { Color = Color.Dark, IsPlayerTurn = true };
             var actual = customGame?.Board?[3, 3].Move((oldCol, oldRow), (newCol, newRow));
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MoveTest_LightPawnMovesDiagonallyIfDarkPawnStandsThere_ReturnsSucceeded()
+        {
+            var move = customGame?.Board?[4, 3].Move((4, 3), (3, 4));
+            var actual = move;
+            var expected = MoveValidationMessage.Succeeded;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MoveTest_DarkPawnMovesDiagonallyIfWhitePawnStandsThere_ReturnsSucceededr()
+        {
+            customGame.Player1 = new() { Color = Color.Light, IsPlayerTurn = false };
+            customGame.Player2 = new() { Color = Color.Dark, IsPlayerTurn = true };
+            var move = customGame?.Board?[3, 4].Move((3, 4), (4, 3));
+            var actual = move;
+            var expected = MoveValidationMessage.Succeeded;
             Assert.AreEqual(expected, actual);
         }
     }
--- ChessAPI/ChessAPI Tests/GamePieces/QueenTests.cs
+++ ChessAPI/ChessAPI Tests/GamePieces/QueenTests.cs
@@ -10,7 +10,7 @@ using SharedCsharpModels.Models;
 namespace ChessAPI.GamePieces.Tests
 {
     [TestClass()]
-    public class RookTests
+    public class QueenTests
     {
         readonly GamesService gamesService = new();
         GameState? customGame;
@@ -22,18 +22,18 @@ namespace ChessAPI.GamePieces.Tests
             customGame.Board = new GamePiece[8, 8]
             {
                 { new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty) },
-                { new NoPiece(customGame, Color.Empty), new Rook(customGame, Color.Light), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new Pawn(customGame, Color.Dark), new NoPiece(customGame, Color.Empty) },
+                { new NoPiece(customGame, Color.Empty), new Queen(customGame, Color.Light), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new Pawn(customGame, Color.Dark), new NoPiece(customGame, Color.Empty) },
                 { new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty) },
                 { new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty) },
                 { new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty) },
                 { new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty) },
-                { new NoPiece(customGame, Color.Empty), new Pawn(customGame, Color.Light), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new Rook(customGame, Color.Dark), new NoPiece(customGame, Color.Empty) },
+                { new NoPiece(customGame, Color.Empty), new Pawn(customGame, Color.Light), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new Queen(customGame, Color.Dark), new NoPiece(customGame, Color.Empty) },
                 { new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty), new NoPiece(customGame, Color.Empty) }
             };
         }
 
         [TestMethod()]
-        public void MoveTest_LightPlayerMovesDarkRook_ReturnsWrongPieceColor()
+        public void MoveTest_LightPlayerMovesDarkQueen_ReturnsWrongPieceColor()
         {
             var move = customGame?.Board?[6, 6].Move((6, 6), (6, 5));
             var actual = move;
@@ -42,7 +42,7 @@ namespace ChessAPI.GamePieces.Tests
         }
 
         [TestMethod()]
-        public void MoveTest_DarkPlayerMovesLightRook_ReturnsWrongPieceColor()
+        public void MoveTest_DarkPlayerMovesLightQueen_ReturnsWrongPieceColor()
         {
             customGame.Player1 = new() { Color = Color.Light, IsPlayerTurn = false };
             customGame.Player2 = new() { Color = Color.Dark, IsPlayerTurn = true };
@@ -53,22 +53,30 @@ namespace ChessAPI.GamePieces.Tests
         }
 
         [TestMethod()]
-        [DataRow(1, 1, 0, 1, MoveValidationMessage.Succeeded)]
+        [DataRow(1, 1, 0, 0, MoveValidationMessage.Succeeded)]
         [DataRow(1, 1, 1, 0, MoveValidationMessage.Succeeded)]
-        [DataRow(1, 1, 2, 1, MoveValidationMessage.Succeeded)]
+        [DataRow(1, 1, 2, 0, MoveValidationMessage.Succeeded)]
         [DataRow(1, 1, 1, 2, MoveValidationMessage.Succeeded)]
-        public void MoveTest_LightRookMovesOneSquareVerticallyOrHorizontally_ReturnsSucceeded(int oldCol, int oldRow, int newCol, int newRow, MoveValidationMessage expected)
+        [DataRow(1, 1, 2, 2, MoveValidationMessage.Succeeded)]
+        [DataRow(1, 1, 1, 2, MoveValidationMessage.Succeeded)]
+        [DataRow(1, 1, 0, 2, MoveValidationMessage.Succeeded)]
+        [DataRow(1, 1, 0, 1, MoveValidationMessage.Succeeded)]
+        public void MoveTest_LightQueenMovesOneSquareAroundSelf_ReturnsSucceeded(int oldCol, int oldRow, int newCol, int newRow, MoveValidationMessage expected)
         {
             var actual = customGame?.Board?[1,1].Move((oldCol, oldRow), (newCol, newRow));
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod()]
+        [DataRow(6, 6, 5, 5, MoveValidationMessage.Succeeded)]
         [DataRow(6, 6, 6, 5, MoveValidationMessage.Succeeded)]
-        [DataRow(6, 6, 7, 6, MoveValidationMessage.Succeeded)]
+        [DataRow(6, 6, 7, 5, MoveValidationMessage.Succeeded)]
+        [DataRow(6, 6, 7, 5, MoveValidationMessage.Succeeded)]
+        [DataRow(6, 6, 7, 7, MoveValidationMessage.Succeeded)]
         [DataRow(6, 6, 6, 7, MoveValidationMessage.Succeeded)]
+        [DataRow(6, 6, 5, 7, MoveValidationMessage.Succeeded)]
         [DataRow(6, 6, 5, 6, MoveValidationMessage.Succeeded)]
-        public void MoveTest_DarkRookMovesOneSquareVerticallyOrHorizontally_ReturnsSucceeded(int oldCol, int oldRow, int newCol, int newRow, MoveValidationMessage expected)
+        public void MoveTest_DarkQueenMovesOneSquareAroundSelf_ReturnsSucceeded(int oldCol, int oldRow, int newCol, int newRow, MoveValidationMessage expected)
         {
             customGame.Player1 = new() { Color = Color.Light, IsPlayerTurn = false };
             customGame.Player2 = new() { Color = Color.Dark, IsPlayerTurn = true };
@@ -78,10 +86,11 @@ namespace ChessAPI.GamePieces.Tests
 
         [TestMethod()]
         [DataRow(1, 1, 1, 1, MoveValidationMessage.IllegalMove)]
-        [DataRow(1, 1, 2, 2, MoveValidationMessage.IllegalMove)]
+        [DataRow(1, 1, 7, 7, MoveValidationMessage.IllegalMove)]
         [DataRow(1, 1, 1, 7, MoveValidationMessage.IllegalMove)]
+        [DataRow(1, 1, 7, 1, MoveValidationMessage.IllegalMove)]
         [DataRow(1, 1, 6, 1, MoveValidationMessage.IllegalMove)]
-        public void MoveTest_LightRookMovesWhenMoveToSameSpotOrPathBlockedOrToOwnColorPieceOrDiagonally_ReturnsIllegalMove(int oldCol, int oldRow, int newCol, int newRow, MoveValidationMessage expected)
+        public void MoveTest_LightQueenMovesWhenMoveToSameSpotOrPathBlockedOrToOwnColorPiece_ReturnsIllegalMove(int oldCol, int oldRow, int newCol, int newRow, MoveValidationMessage expected)
         {
             var actual = customGame?.Board?[1, 1].Move((oldCol, oldRow), (newCol, newRow));
             Assert.AreEqual(expected, actual);
@@ -89,10 +98,11 @@ namespace ChessAPI.GamePieces.Tests
 
         [TestMethod()]
         [DataRow(6, 6, 6, 6, MoveValidationMessage.IllegalMove)]
+        [DataRow(6, 6, 0, 0, MoveValidationMessage.IllegalMove)]
         [DataRow(6, 6, 0, 6, MoveValidationMessage.IllegalMove)]
-        [DataRow(6, 6, 5, 5, MoveValidationMessage.IllegalMove)]
+        [DataRow(6, 6, 6, 0, MoveValidationMessage.IllegalMove)]
         [DataRow(6, 6, 1, 6, MoveValidationMessage.IllegalMove)]
-        public void MoveTest_DarkRookMovesWhenMoveToSameSpotOrPathBlockedOrToOwnColorPieceOrDiagonally_ReturnsIllegalMove(int oldCol, int oldRow, int newCol, int newRow, MoveValidationMessage expected)
+        public void MoveTest_DarkQueenMovesWhenMoveToSameSpotorPathBlockedOrToOwnColorPiece_ReturnsIllegalMove(int oldCol, int oldRow, int newCol, int newRow, MoveValidationMessage expected)
         {
             customGame.Player1 = new() { Color = Color.Light, IsPlayerTurn = false };
             customGame.Player2 = new() { Color = Color.Dark, IsPlayerTurn = true };
@@ -101,7 +111,7 @@ namespace ChessAPI.GamePieces.Tests
         }
 
         [TestMethod()]
-        public void MoveTest_LightRookMovesToDarkPawn_ReturnsSucceeded()
+        public void MoveTest_LightQueenMovesToDarkPawn_ReturnsSucceeded()
         {
             var move = customGame?.Board?[1, 1].Move((1, 1), (1, 6));
             var actual = move;
@@ -110,7 +120,7 @@ namespace ChessAPI.GamePieces.Tests
         }
 
         [TestMethod()]
-        public void MoveTest_DarkRookMovesToLightPawn_ReturnsSucceeded()
+        public void MoveTest_DarkQueenMovesToLightPawn_ReturnsSucceeded()
         {
             customGame.Player1 = new() { Color = Color.Light, IsPlayerTurn = false };
             customGame.Player2 = new() { Color = Color.Dark, IsPlayerTurn = true };
